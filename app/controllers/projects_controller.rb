54bc34ed13a97745cb53488c3e43a794d7442026
John Doe <cjc10@192.168.1.115>
2016-06-25 15:57:39 -0500

    segundo commit, cambios en proceso, completar parte de los proyectos

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,17 +4,20 @@ class ProjectsController < ApplicationController
 		@proyectos_empleado= employee.projects
 	end
 	def show
+		employee=Employee.find(params[:employee_id])
 		@project=Project.find(params[:project_id])
 	end
 	def new
+		employee=Employee.find(params[:employee_id])
 		@project=Project.new
 	end
 	def create
+		employee=Employee.find(params[:employee_id])
   		@project=Project.create(project_params)
   		if @project.save
   			redirect_to @project, flash:{success: "Empleado resgistrado exitosamente"}
   		else
-  		redirect_to "/project/new", flash:{error:'Hubo un error, intentelo nuevamente'}
+  		redirect_to "<%=employee.id%>/project/new", flash:{error:'Hubo un error, intentelo nuevamente'}
   		end
   	end
 end
